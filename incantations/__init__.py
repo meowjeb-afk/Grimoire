e4b8abf303dabe0f388f5b298e9333b6c1860e3d
meowjeb-afk <meowjeb@gmail.com>
2026-06-04 17:36:43 +0100

    Update __init__.py

--- incantations/__init__.py
+++ incantations/__init__.py
@@ -1,1 +1,14 @@
+# File: incantations/__init__.py
 
+from .file_alchemy import *
+from .asset_summoner import *
+from .clipboard_magic import *
+from .workspace_stasis import *
+from .text_expansion import *
+from .updater_scryer import *
+from .purge_debloat import *
+from .arcane_intel import *
+from .scry_search import *
+from .void_shield import *
+from .ecosystem_summoner import *
+from .image_matrix import *
